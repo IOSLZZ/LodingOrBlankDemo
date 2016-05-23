dc5a5a6c284160457003e599af4553c3c574c40d
LZZ <814232148@qq.com>
2016-05-23 17:28:51 +0800

    Initial Commit

---
+++ LodingOrBlankDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LodingOrBlankDemo
+//
+//  Created by admin on 16/5/23.
+//  Copyright © 2016年 LZZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
